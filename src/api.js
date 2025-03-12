ce69bf046446dea51221ee1bed6d153590766cf4
hyouzl <hyouzl0521@gmail.com>
2025-03-13 06:05:22 +0900

    error 처리 수정

--- src/api.js
+++ src/api.js
@@ -45,6 +45,11 @@ api.interceptors.response.use(
       return Promise.reject(error);
     }
 
+    if (error.response?.data === "Refresh Token is missing") {
+      alert("다시 로그인을 해주세요!");
+      window.location.href = "/users/login";
+    }
+
     if (error.response.data?.code === "COMMON401") {
       alert("아이디와 비밀번호를 확인해주세요.");
       window.location.href = "/users/login";
